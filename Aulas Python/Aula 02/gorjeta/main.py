bda3546cadb8d7d243f39a133962d5b83deddb25
Jean Barbosa <jeanbarbosa.it@gmail.com>
2025-09-11 21:35:30 -0300

    Praticando Python aula 02.

---
+++ Aulas Python/Aula 02/gorjeta/main.py
@@ -0,0 +1,12 @@
+from calcular import valor_total_gorjeta
+from calcular import valor_total_pagar
+ 
+total_conta = float(input('Digite o valor da conta: '))
+porcentagem = float(input('Digite a porcentagem de gorjeta: '))
+valor_final = valor_total_gorjeta(total_conta,porcentagem)
+
+gorjeta = valor_total_gorjeta(total_conta,porcentagem)
+valor_total = valor_total_pagar(total_conta,gorjeta)
+
+print(f'Valor da gorjeta: R$ {round(gorjeta,2)}')
+print(f'Total a pagar: R$ {round(valor_total,2)}')
