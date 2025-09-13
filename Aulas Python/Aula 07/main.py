3ff6f6841bde6323b5c614efc089deadc175a039
Jean Barbosa <jeanbarbosa.it@gmail.com>
2025-09-13 00:34:41 -0300

    jogo pedra papel e tesoura.

---
+++ Aulas Python/Aula 07/main.py
@@ -0,0 +1,11 @@
+from validacoes.funcoes import limpar_tela
+from validacoes.funcoes import titulo_jogo
+from validacoes.funcoes import jogar
+
+def main():
+    titulo_jogo()
+    jogar()
+
+if __name__ == "__main__":
+    limpar_tela()
+    main()
